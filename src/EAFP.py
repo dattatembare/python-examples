38345f2144bfc4cabfdfef88405154d84cad6a6b
Dattatraya Tembare <dtembare@solutechnology.com>
2019-08-08 22:37:25 -0400

    Duck typing and eapf

--- src/EAFP.py
+++ src/EAFP.py
@@ -2,32 +2,32 @@
 
 my_list = [1, 2, 3, 4, 5]
 
-# Non pythonic
+# Non pythonic (LBYL - Look before you leap)
 if len(my_list) > 5:
     print(my_list[5])
 else:
     print('Index does not exist')
 
-# Pythonic
+# Pythonic (EAFP)
 try:
     print(my_list[5])
 except:
     print('Index does not exist')
 
-print('---------------------------------')
+print('*************************************')
 
 import os
 
 my_file = "/path/to/my/file.txt"
 
-# Race condition (non pythonic)
+# Race condition (non pythonic)/LBYL
 if os.access(my_file, os.R_OK):
     with open(my_file) as f:
         print(f.read())
 else:
     print("File can't be accessed")
 
-# No race condition (pythonic)
+# No race condition (pythonic)/EAFP
 try:
     f = open(my_file)
 except IOError as e:
