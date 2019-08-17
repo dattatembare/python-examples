948c1551e963581e1a2313349452af076c4bfc0a
Dattatraya Tembare <dtembare@solutechnology.com>
2019-08-17 13:05:53 -0400

    restructuring examples

--- src/dictionary_stuff.py
+++ src/dictionary_stuff.py
@@ -1,3 +1,8 @@
+d = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+print(d)
+print(d.popitem())
+print(d)
+
 # Merge two dictionaries
 x = {'a': 1, 'b': 2}
 y = {'b': 3, 'c': 4}
@@ -6,8 +11,7 @@ print(z)  # {'a': 1, 'b': 3, 'c': 4}
 zz = {**y, **x}
 print(zz)  # {'b': 2, 'c': 4, 'a': 1}
 
-# How to sort a Python dict by value
-# (== get a representation sorted by value)
+# How to sort a Python dict by value (== get a representation sorted by value)
 
 xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
 
@@ -41,5 +45,25 @@ def greeting(userid):
 
 
 print(greeting(382))  # "Hi Alice!"
-
 print(greeting(333333))  # "Hi there!"
+
+#  Inverting a dictionary using zip
+m = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+print(m.items())  # [('a', 1), ('c', 3), ('b', 2), ('d', 4)]
+print(dict(zip(m.values(), m.keys())))  # {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
+
+# Inversing a dictionary using dict comprehensions
+m1 = {v: k for k, v in m.items()}
+print(m1)
+
+# Deleting element from dictionary
+print(m.pop('a'))
+print(m)
+print(m.clear())
+
+seq = ('name', 'age', 'sex')
+dict = dict.fromkeys(seq)
+print(f"New Dictionary : {dict}")
+
+dict = dict.fromkeys(seq, 'null')  # New Dictionary : {'name': None, 'age': None, 'sex': None}
+print(f"New Dictionary : {str(dict)}")  # New Dictionary : {'name': 'null', 'age': 'null', 'sex': 'null'}
--- src/list_stuff.py
+++ src/list_stuff.py
@@ -2,3 +2,20 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for a, b, c in zip(*[iter(my_list)] * 3):
     print(a, b, c)
 
+a, *b, c = [1, 2, 3, 4, 5]
+print(a, b, c)
+
+a = [0, 1, 2, 3, 4, 5]
+LASTTHREE = slice(-3, None)
+print(LASTTHREE)  # slice(-3, None, None)
+print(a[LASTTHREE])  # [3, 4, 5]
+print(a[-3::])  # [3, 4, 5]
+
+##########################################################
+#  Flattening lists:
+# Note: according to Python's documentation on sum, itertools.chain.from_iterable is the preferred method for this.
+import itertools
+
+a = [[1, 2], [3, 4], [5, 6]]
+print(list(itertools.chain.from_iterable(a)))  # [1, 2, 3, 4, 5, 6]
+print(sum(a, []))  # [1, 2, 3, 4, 5, 6]
