96d6790806d8cb631ee258739f589e2ce08152f5
Dattatraya Tembare <dtembare@solutechnology.com>
2019-09-30 10:10:07 -0400

    find minimum number of coins to return as a change, added more scenarios

--- src/coding_interview/min_coins_change.py
+++ src/coding_interview/min_coins_change.py
@@ -1,12 +1,17 @@
 def number_of_coins(centes_in, coins):
     coins_dict = {}
-    centes = centes_in
     for coin in coins:
-        if coin <= centes:
-            coins_dict[coin] = centes // coin
-            centes = centes % coin
+        if coin <= centes_in:
+            coins_dict[coin] = centes_in // coin
+            centes_in = centes_in % coin
     return coins_dict
 
 
 available_coins = [25, 10, 5, 1]
-print(number_of_coins(42, available_coins))
+print(number_of_coins(42, available_coins))  # {25: 1, 10: 1, 5: 1, 1: 2}
+
+available_coins = [25, 10, 1]  # Nickles out of stock
+print(number_of_coins(42, available_coins))  # {25: 1, 10: 1, 1: 7}
+
+available_coins = [25, 5, 1]  # Dimes out of stock
+print(number_of_coins(42, available_coins))  # {25: 1, 10: 1, 5: 1, 1: 2}
